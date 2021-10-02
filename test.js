e684715fc229e7ebf81880b8acd02d4169185d4b
Levon97 <levon.samsonyan@gmail.com>
2021-10-02 18:28:02 +0400

    adding custom Error classes ,adding errorhandler in base Controller , and now users password not giveing back with response

---
+++ test.js
@@ -0,0 +1,40 @@
+const PassConverter = require('./helpers/PassConverter');
+
+// class MyError extends Error{
+//     constructor(message){
+//         super(message);
+//         this.name = "MyError";
+        
+//     }
+// }
+// const k ={
+//     gago: "gaaag"
+// }
+
+
+// function errorHandler (error){
+//     if(error.massage === "gagulik"){
+//         console.log("gago mago");
+//     }
+// }
+
+// function errorer(){
+//     try {
+//         throw new MyError("gagulik");
+//     } catch (error) {
+//         console.log(error.type);
+    
+//     }
+// }
+
+// errorer();
+
+async function gag( ){
+    console.log(await PassConverter.hash("gag"));
+    
+}
+
+
+
+
+gag()
