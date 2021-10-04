8e290386ad5de586fcc65566abbc562b34eaa580
Levon97 <levon.samsonyan@gmail.com>
2021-10-04 12:18:50 +0400

    adding profile.logout and bug fixes

--- routes/authRouts.js
+++ routes/authRouts.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const AuthController = require('../controllers/AuthController')
 const authController = new AuthController();
+
 router.post('/sign-up', authController.registration);
 router.post('/sign-in',authController.logIn);
 
