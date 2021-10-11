d1a5ef15ce1988de2e653b9d64e3455bc72cec5f
Levon97 <levon.samsonyan@gmail.com>
2021-10-11 12:52:28 +0400

    contants edited

--- config/constants.js
+++ config/constants.js
@@ -1,15 +1,12 @@
-const awards = {
-    '1': "1000 USD first constant prize",
-    '2': "32000 usd second constant prize",
-    '3': "1000000 usd maximum prize"
+const constants = {
+    awards: {
+        '1': "1000 USD first constant prize",
+        '2': "32000 usd second constant prize",
+        '3': "1000000 usd maximum prize"
+    },
+    firstImmutableQuestion: 5,
+    secondImmutableQuestion: 10,
+    lastQuestion: 15
 }
-const firstImmutableQuestion = 5;
-const secondImmutableQuestion = 10;
-const lastQuestion = 15;
 
-module.exports = {
-    awards,
-    firstImmutableQuestion,
-    secondImmutableQuestion,
-    lastQuestion
-}
+module.exports = constants;
